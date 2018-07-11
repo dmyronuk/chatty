940f5b206cb0a0e4b9fa934efb783acb2130d949
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-07-11 17:25:32 +0000

    Image urls typed into chat bar now display images Currently does not handle when users type multiple images into the same message

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
 function Message(props){
+
+  let regex = /https?:\/\/\S+\.(?:jpg|jpeg|png|gif)/g;
+  let imageURL = props.content.match(regex);
+  //if there's text plus an image url, splice the image url out of the text content
+  let parsedContent = imageURL ? props.content.replace(imageURL, "") : props.content;
+
   return (
     <div className="message">
-      <span className={`message-username ${props.msgColorClass}`}>{props.username}</span>
-      <span className="message-content">{props.content}</span>
+      <div className={`message-username ${props.msgColorClass}`}>{props.username}</div>
+      <div className="message-content">
+        <div>
+          {parsedContent}
+        </div>
+        <div>
+          {imageURL &&
+            <img className="chat-img" src={imageURL} />
+          }
+        </div>
+      </div>
     </div>
   )
 }
